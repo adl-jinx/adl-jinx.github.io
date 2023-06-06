5a01414a8e66461dfb0be08ed59b6b337da61ad1
adl-aws <dl.antonio@icloud.com>
2023-06-06 17:31:21 +0000

    This is the first commit, just to save the data.

---
+++ app.py
@@ -0,0 +1,29 @@
+# gunicorn -b 0.0.0.0:8000 app:app --> Command to run the server
+# use host = "0.0.0.0/0"
+
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    return render_template("home.html")
+
+
+@app.route('/resume')
+def resume():
+    return render_template("resume.html")
+
+@app.route('/projects')
+def projects():
+    return render_template("projects.html")
+
+
+
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0:8000", debug=False)
+
+# http://44.235.104.249: this is the public ip address
